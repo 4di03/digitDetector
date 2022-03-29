7542070e53c8743101acdcf73347ca8fcb299ae4
4di03 <adithyapalle12@gmail.com>
2022-03-29 01:07:37 -0400

    almsmot works

--- textDetect.py
+++ textDetect.py
@@ -5,17 +5,20 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
+model = tf.keras.models.load_model('saved_models/model1')
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
-assert x_train.shape == (60000, 28, 28)
-assert x_test.shape == (10000, 28, 28)
-assert y_train.shape == (60000,)
-assert y_test.shape == (10000,)
 
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+
+#plt.imshow(x_test[3])
 
 def predict(img):
-    return 1
+    img = cv2.resize(img, (28, 28), interpolation = cv2.INTER_AREA)
+    img = np.expand_dims(img, axis=0)
+    img = img *255
 
-
+    #print(img)
+    return np.argmax(model.predict(img))
 
 
 
@@ -29,14 +32,14 @@ def draw(event, x, y, flag,  param):
         run = False
 
     if event == cv2.EVENT_MOUSEMOVE and run:
-       cv2.circle(win, (x,y), 5 , 0 , -1)
+       cv2.circle(win, (x,y), 10 , 1 , -1)
 
     
 #create blank windows for drwawing
 cv2.namedWindow('window')
 cv2.setMouseCallback('window', draw)
 
-win = np.ones((280, 280, 1) , dtype = 'float64')
+win = np.zeros((280, 280, 1) , dtype = 'float64')
 win2 = cv2.putText(np.zeros((500, 700, 3), dtype = 'float64'), 'Press s to save image for Prediction!', (50, 30), cv2.FONT_HERSHEY_SIMPLEX,1, (255, 255, 255), 1, cv2.LINE_AA)
 
 win2 = cv2.putText(win2, 'Neural Net Prediction:', (50, 80), cv2.FONT_HERSHEY_SIMPLEX,1, (255, 255, 255), 1, cv2.LINE_AA)
@@ -51,11 +54,13 @@ while True:
     k = cv2.waitKey(1)
 
     if k == ord('c'):
-        win = np.ones((280, 280, 1) , dtype = 'float64')
+        win = np.zeros((280, 280, 1) , dtype = 'float64')
 
     if k == ord('s'):
         win2[120:400, 20:300] = win
-        win2 = cv2.putText(win2, str(predict(win)), (350, 400), cv2.FONT_HERSHEY_SIMPLEX, 15, (0, 0, 255), 15, cv2.LINE_AA)
+        win2[50:500, 350:700] = 0
+        win2 = cv2.putText(win2, str(predict(win)), (350, 390), cv2.FONT_HERSHEY_SIMPLEX, 15, (0, 0, 255), 15, cv2.LINE_AA)
+        
 
 
     if k == 27:
